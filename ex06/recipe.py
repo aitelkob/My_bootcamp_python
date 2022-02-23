0279a3b0add970ed6f1794378fd07e8bee5ec55a
Yait-el- <yait-el-@student.1337.ma>
2022-02-23 22:15:01 +0100

    still in day06 😒

--- ex06/recipe.py
+++ ex06/recipe.py
@@ -3,13 +3,30 @@ import sys
 cookbook = {}
 
 def print_recipe_from_cookbook(recipe):
-    pass
+    if recipe in cookbook.keys():
+        print("Recipe for {}:".format(recipe))
+        print("Ingredient list: {}".format(cookbook[recipe][0]))
+        print("To be eaten for {}.".format(cookbook[recipe][1]))
+        print("Takes {} minutes of cooking.".format(cookbook[recipe][2]))
+    else:
+        print("Invalid recipe name.")
+
 
 def del_recipe(recipe):
-    pass
+    if recipe in cookbook.keys():
+        del cookbook[recipe]
+        print("Deleted {} recipe.".format(recipe))
+    else:
+        print("Invalid recipe name.")
 
 def add_recipe(recipe,ingredients, meal ,prep_time):
-    pass
+    if recipe in cookbook.keys():
+        print("'{}' already exists, overwriting...".format(recipe))
+    cookbook[recipe] = (ingredients, meal.prep_time)
+    print("Added '{}' to cookbook.".format(recipe))
 
 def print_all_recipe():
-    pass
+    [print_recipe_from_cookbook(recipe) for recipe in cookbook.keys()]
+
+
+chois = 0
