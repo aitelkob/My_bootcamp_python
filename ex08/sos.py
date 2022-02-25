b47035b8041e36c4513e8908d91b16bd14cda102
Yait-el- <aitelkob@users.noreply.github.com>
2022-02-25 17:34:41 +0100

    ex08

--- ex08/sos.py
+++ ex08/sos.py
@@ -19,17 +19,19 @@ dict_code = { 'A':'.-', 'B':'-...',
 
 
 #chek_if char have == key in dict
+code = ""
 def mourse(text):
-    sentence = sys.argv[1].upper()
+    sentence = text.upper()
+    print(sentence)
     mourse = ""
     for char in sentence:
         mourse+=dict_code[char] + " "
     return mourse
 if len(sys.argv) > 1:
-    str = sys.argv[1]
+    str = str(sys.argv[1:])
     for i in range(len(sys.argv) - 2):
         str = str + " " + sys.argv[i + 2]
-    #mourse
+    code = mourse(str)
 
 
-print(mourse)
+print(code)
