18368999020701b43faed13f92f83652f2464051
Yait-el- <aitelkob@users.noreply.github.com>
2022-03-04 18:04:04 +0100

    Day01/ex01

--- day01/ex00/book.py
+++ day01/ex00/book.py
@@ -6,7 +6,7 @@ class Book:
         self.name = name
         self.last_update = datetime.datetime.now()
         self.creation_date = datetime.datetime.now()
-        self.creation_list = {'starter': [], 'lunch': [], 'dessert': []}
+        self.recipes_list = {'starter': [], 'lunch': [], 'dessert': []}
     def __str__(self):
         txt = ("Recipe book's name : " + self.name + "\n"
             + "Last update : " + str(self.last_update) + "\n"
@@ -14,7 +14,7 @@ class Book:
             + "Starters : " + str(self.recipes_list['starter']) + "\n"
             + "Lunches : " + str(self.recipes_list['lunch']) + "\n"
             + "Desserts : " + str(self.recipes_list['dessert']))
-    return txt
+        return txt
     def get_recipe_by_name(self, name):
         try:
             for lst in self.creation_list.values():
@@ -27,10 +27,35 @@ class Book:
                             
         except (Exception,ValueError) as err:
                print("Error: " ,err)
-# first_book = Book("First book")
-# Book.get_recipes_by_types(first_book, 'starter')
+    def get_recipes_by_types(self, recipes_type):
+        try:
+            if recipes_type in self.recipes_list:
+                print("Recipe in " + recipes_type + ":")
+                for elem in self.recipes_list[recipes_type]:
+                    print(elem.name)
+                return
+            else:
+                raise Exception("Couldn't find the Recipe type ")
+        except (Exception,ValueError) as err:
+            print("Error:" ,err)
+    def add_recipe(self, recipe):
+        try:
+            if recipe.recipe_type in self.recipes_list:
+                self.recipes_list[recipe.recipe_type].append(recipe)
+                self.last_update = datetime.datetime.now()
+            else:
+                raise Exception("Recipe type isn't starter,lunch or dessert")
+        except (Exception, ValueError) as err:
+            print("Error:",err)
 
-# print(first_book.name)
-# print(first_book.last_update)
-# print(first_book.creation_date)
-# print(first_book.recipes_list)
+
+
+#first_book = Book("First book")
+#Book.get_recipes_by_types(first_book, 'starter')
+
+#print(first_book.name)
+#print(first_book.last_update)
+#print(first_book.creation_date)
+#print(first_book.recipes_list)
+#to_print = str(first_book)
+#print(to_print)
--- day01/ex00/test.py
+++ day01/ex00/test.py
@@ -1,6 +1,6 @@
 from recipe import Recipe
 from book import Book
-
+import time
 
 if __name__ == '__main__':
     # Create Recipe :
@@ -17,15 +17,24 @@ if __name__ == '__main__':
 
     ### Testing :
     # Add Recipe to Book
-    first_book.add_recipe(tourte)
-    first_book.add_recipe(smoothie)
-
+    
+    #first_book.add_recipe(tourte)
+    #first_book.add_recipe(smoothie)
     # Add invalid Recipe to Book :
     # Book.add_recipe(first_book, 6)
-
+    b = Book("My seductive recipes")
+    print(b.creation_date)
+    # should be the current date and time
+    print(b.last_update)
+    time.sleep(1.2)
+    print("exampple ==============1 \n")
+    crumble = Recipe("Crumble" , 1, 25, ["apples", "flour", "sugar"], "dessert" ,"delicious")
+    b.add_recipe(crumble)
+    print(b.last_update)
+    # should be the same as the creation date or None
     # Find recipes in Book by type :
-    Book.get_recipes_by_types(first_book, 'lunch')
-    print("")
+    #Book.get_recipes_by_types(first_book, 'lunch')
+    #print("")
 
     # Print recipe from Book :
-    print(smoothie)
+    #print(smoothie)
