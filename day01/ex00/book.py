6d46b2adc6080727a04e5342ef378bf5d2b85ea2
Yait-el- <yait-el-@student.1337.ma>
2022-03-04 14:54:03 +0100

    ex00 need testing

--- day01/ex00/book.py
+++ day01/ex00/book.py
@@ -7,6 +7,14 @@ class Book:
         self.last_update = datetime.datetime.now()
         self.creation_date = datetime.datetime.now()
         self.creation_list = {'starter': [], 'lunch': [], 'dessert': []}
+    def __str__(self):
+        txt = ("Recipe book's name : " + self.name + "\n"
+            + "Last update : " + str(self.last_update) + "\n"
+            + "Creation date : " + str(self.creation_date) + "\n"
+            + "Starters : " + str(self.recipes_list['starter']) + "\n"
+            + "Lunches : " + str(self.recipes_list['lunch']) + "\n"
+            + "Desserts : " + str(self.recipes_list['dessert']))
+    return txt
     def get_recipe_by_name(self, name):
         try:
             for lst in self.creation_list.values():
@@ -19,6 +27,10 @@ class Book:
                             
         except (Exception,ValueError) as err:
                print("Error: " ,err)
+# first_book = Book("First book")
+# Book.get_recipes_by_types(first_book, 'starter')
 
-
-
+# print(first_book.name)
+# print(first_book.last_update)
+# print(first_book.creation_date)
+# print(first_book.recipes_list)
--- day01/ex00/test.py
+++ day01/ex00/test.py
@@ -1,10 +1,31 @@
-test = 2
-cooking_lvl = 2
-test2 = 2
-try:
-    a = int(input("Enter a positive Number: "))
-    if not  a in {test2,test,cooking_lvl}:
-        raise ValueError("it's not a positive number!")
-except ValueError as ve:
-    print(ve)
+from recipe import Recipe
+from book import Book
 
+
+if __name__ == '__main__':
+    # Create Recipe :
+    listy = ['Pate', 'Saumon', 'Epinard']
+    tourte = Recipe("Tourte", 3, 20, listy, "", "lunch")
+
+    # Create second Recipe :
+    listy = ['Miel', 'Banane', 'Yaourt']
+    smoothie = Recipe("Smoothie miel-banane", 3, 10, listy,
+            "Un smoothie de yaourt au miel et à la banane", "dessert")
+
+    # Create Book :
+    first_book = Book("First book")
+
+    ### Testing :
+    # Add Recipe to Book
+    first_book.add_recipe(tourte)
+    first_book.add_recipe(smoothie)
+
+    # Add invalid Recipe to Book :
+    # Book.add_recipe(first_book, 6)
+
+    # Find recipes in Book by type :
+    Book.get_recipes_by_types(first_book, 'lunch')
+    print("")
+
+    # Print recipe from Book :
+    print(smoothie)
